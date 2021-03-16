45dbc39eae5b5222a22b837038a84e712f1ec9db
Gabriel <gabrieldelahaye76680@gmail.com>
2021-03-16 15:33:02 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,50 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="list-group">
-                        @if(count($stats) > 0)
-                        @foreach ($stats as $stat)
-
-                        <div class="flex-column align-items-start">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h5 class="mb-1 text-gray-800 font-weight-bold"> {{ $stat->ip_address }} </h5>
-                            </div>
-                            <p class="mb-1">{{ $stat->action }}</p>
-                            <div class="d-flex w-100 justify-content-between">
-                                <small style="margin: 0 0 0 auto;"><span class="badge badge-secondary" style="font-size: 12px;">{{ $stat->created_at}}</span></small>
-                            </div>
-                        </div>
-                        <hr style="margin: 15px;">
-
-                        @endforeach
-                        @else
-
-                        <p>No recent activity.</p>
-
-                        @endif
-                    </div>
+                {{ count($stats) . " connections today"}}
                 </div>
             </div>
         </div>
-        <script>
-            function readURL(input) {
-                if (input.files && input.files[0]) {
-                    var reader = new FileReader();
-
-                    reader.onload = function(e) {
-                        $('#preview').attr('src', e.target.result);
-                    }
-
-                    imageLabel.textContent = input.files[0]["name"];
-                    reader.readAsDataURL(input.files[0]); // convert to base64 string
-                }
-            };
-
-            var imageLabel = document.getElementById('imgLabel');
-            var imageFile = document.getElementById("image");
-            imageFile.addEventListener("change", function() {
-                readURL(this);
-            });
-        </script>
     </div>
 </x-app-layout>
